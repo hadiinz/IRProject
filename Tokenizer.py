9e18e77234c72a2dd4af3cca8f628a3388996875
Hadi Nazemi <9730003@ceit.local>
2024-01-27 19:19:16 +0330

    adding normalization and tokenizer

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,1 +1,25 @@
-print("hello")
+import json
+from stopwordsiso import stopwords
+from parsivar import Normalizer, Tokenizer, FindStems
+
+
+#  preprocessing
+# normalizer
+my_normalizer = Normalizer()
+# tokenizer
+my_tokenizer = Tokenizer()
+# stemmer
+my_stemmer = FindStems()
+# stop words
+persian_stopwords = stopwords("fa")
+# print(persian_stopwords)
+
+# opening JSON file
+f = open('IR_data_news_12k.json')
+# JSON object as a dictionary
+documents = json.load(f)
+f.close()
+
+positional_index = {}
+
+list_stem_stopword_token_normal = []
